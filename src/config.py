8492fbb688cb997661dbdeba80f8264a3bbbe772
Aymane Hachcham <aymanehachchaming@gmail.com>
2023-09-29 23:37:27 +0200

    loading and downloading for whisper complete

--- src/config.py
+++ src/config.py
@@ -4,15 +4,34 @@ import requests
 from tqdm import tqdm
 from subprocess import Popen
 from pathlib import Path
-from pydantic import BaseModel
 import logging
+from whispercpp import Whisper, api
+import toml
 
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
-
-class HuggingFaceModel(BaseModel):
-    name: str
-    path: str
+_WHISPER_AVAILABLE_MODELS = [
+    "tiny.en",
+    "tiny",
+    "tiny-q5_1",
+    "tiny.en-q5_1",
+    "base.en",
+    "base",
+    "base-q5_1",
+    "base.en-q5_1",
+    "small.en",
+    "small.en-tdrz",
+    "small",
+    "small-q5_1",
+    "small.en-q5_1",
+    "medium",
+    "medium.en",
+    "medium-q5_0",
+    "medium.en-q5_0",
+    "large-v1",
+    "large",
+    "large-q5_0",
+]
 
 class ModelConfig:
 
@@ -25,7 +44,7 @@ class ModelConfig:
         pass
 
     @classmethod
-    def download_whisper_model(
+    def _download_whisper_model_sh(
             cls,
             model_name: str = 'small.en',
     ) -> None:
@@ -37,14 +56,14 @@ class ModelConfig:
         ).wait()
 
     @classmethod
-    def download_whisper(
+    def _download_whisper_model(
             cls,
-            model_name: str = 'small.en',
+            model_name: str,
     ):
         """
         Download whisper model from huggingface, following a specific routine.
-        :param model_name:
-        :return: None
+        :param model_name: The name of the model
+        :return: Optional[str] The name of the model if it was downloaded successfully, None otherwise.
         """
         full_model_path = os.path.join(cls._get_model_dir(), f'ggml-{model_name}.bin')
         url = 'https://huggingface.co/ggerganov/whisper.cpp/resolve/main/ggml'
@@ -67,7 +86,7 @@ class ModelConfig:
 
         total_size = int(res.headers.get('content-length', 0))
         with tqdm(total=total_size, unit='iB', unit_scale=True) as pbar:
-            with open(f'ggml-{model_name}.bin', 'wb') as f:
+            with open(full_model_path, 'wb') as f:
                 for chunk in res.iter_content(chunk_size=1024):
                     if chunk:
                         f.write(chunk)
@@ -76,7 +95,34 @@ class ModelConfig:
         logging.info(f'Model {model_name} downloaded successfully. \n')
         return
 
+    @classmethod
+    def load_pretrained_whisper(
+            cls,
+            model_name: str = 'small.en',
+    ) -> Whisper:
+        """
+        Load a pretrained whisper model from huggingface.
+        :param config: The model configuration.
+        :return: A Speech2Transcription object.
+        """
+        if model_name not in _WHISPER_AVAILABLE_MODELS:
+            raise ValueError(f'Model {model_name} not available. Available models are: {_WHISPER_AVAILABLE_MODELS}')
+
+        cls._download_whisper_model(model_name)
+        try:
+            full_path = os.path.join(cls._get_model_dir(), f'ggml-{model_name}.bin')
+        except FileNotFoundError:
+            logging.error(f'Model {model_name} not found. \n')
+            return
+
+        whisper_config = toml.load('../config/whisper_config.toml')
+        return Whisper.from_pretrained(model_name=full_path)
+
+
+
+
+
 
 
 if __name__ == '__main__':
-    ModelConfig.download_whisper(model_name='tiny.en')
+    model = ModelConfig.load_pretrained_whisper('tiny.en')
