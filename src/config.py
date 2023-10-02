ed9b0dd93de2150efb9a13b86a1394650c090bcf
Aymane Hachcham <aymanehachchaming@gmail.com>
2023-10-02 17:38:33 +0200

    updates

--- src/config.py
+++ src/config.py
@@ -1,27 +1,30 @@
-
 import os
 import logging
-import toml
 import hashlib
+import sys
+
 import requests
+import toml
+
 from pathlib import Path
+from typing import Literal
 from whispercpp import Whisper
 from tqdm import tqdm
-from typing import Literal
+
 from utils import get_model_size
 from config_schema import CFGProgram, WhisperSerial
 
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
 
+
 class ModelConfig:
+    """
+    Handles and correctly loads the models from the local disk.
+    """
 
     @classmethod
-    def __get_dir(
-            cls,
-            config: CFGProgram,
-            dirname: Literal['models', 'config']
-    ):
+    def __get_dir(cls, config: CFGProgram, dirname: Literal["models", "config"]):
         """
         Get the directory path for the models or config folder.
         :param config: The config object for the whole program
@@ -33,12 +36,11 @@ class ModelConfig:
             dir_path.mkdir(parents=True)
         return dir_path
 
-
     @classmethod
     def _download_whisper_model(
-            cls,
-            model_name: str,
-            config: CFGProgram,
+        cls,
+        model_name: str,
+        config: CFGProgram,
     ):
         """
         Download whisper model from huggingface, following a specific routine.
@@ -46,35 +48,35 @@ class ModelConfig:
         :param config: The config object for the whole program
         :return: None
         """
-        full_model_path = os.path.join(cls.__get_dir(config, 'models'), f'ggml-{model_name}.bin')
-        full_config_path = os.path.join(cls.__get_dir(config, 'config'), f'ggml-{model_name}.toml')
+        full_model_path = os.path.join(cls.__get_dir(config, "models"), f"ggml-{model_name}.bin")
+        full_config_path = os.path.join(cls.__get_dir(config, "config"), f"ggml-{model_name}.toml")
         url = config.models.whisper.download_url
-        available_models = {name: checksum for name, checksum in config.general.available_whisper_models}
+        available_models = dict(config.general.available_whisper_models)
 
-        if not model_name in available_models.keys():
-            logging.error(f'Model {model_name} not available. Available models are: {available_models.keys()} \n')
+        if model_name not in available_models.keys():
+            logging.error(f"Model {model_name} not available. Available models are: {available_models.keys()} \n")
             return
 
         if os.path.exists(full_model_path):
-            logging.info(f'Model {model_name} already exists. Skipping download \n')
+            logging.info(f"Model {model_name} already exists. Skipping download \n")
             return
 
-        logging.info(f'Downloading model {model_name} from {url} ... \n')
+        logging.info(f"Downloading model {model_name} from {url} ... \n")
         try:
-            res = requests.get(f'{url}-{model_name}.bin', stream=True)
+            res = requests.get(f"{url}-{model_name}.bin", stream=True)
             res.raise_for_status()
         except requests.RequestException as e:
-            logging.error(f'Error while downloading model {model_name} from {url} \n')
+            logging.error(f"Error while downloading model {model_name} from {url} \n")
             logging.error(e)
             return
 
         # Create a temp file to validate the checksum
         hasher = hashlib.sha1()
-        temp_file_path = f'{full_model_path}.tmp'
+        temp_file_path = f"{full_model_path}.tmp"
 
-        total_size = int(res.headers.get('content-length', 0))
-        with tqdm(total=total_size, unit='iB', unit_scale=True) as pbar:
-            with open(temp_file_path, 'wb') as temp_file:
+        total_size = int(res.headers.get("content-length", 0))
+        with tqdm(total=total_size, unit="iB", unit_scale=True) as pbar:
+            with open(temp_file_path, "wb") as temp_file:
                 for chunk in res.iter_content(chunk_size=8192):
                     if chunk:
                         hasher.update(chunk)
@@ -82,39 +84,38 @@ class ModelConfig:
                         pbar.update(len(chunk))
 
         if hasher.hexdigest() != available_models[model_name]:
-            logging.error(f'Checksum of the downloaded file does not match the expected value. \n')
-            os.remove(temp_file_path) # Remove the temp file
+            logging.error("Checksum of the downloaded file does not match the expected value. \n")
+            os.remove(temp_file_path)  # Remove the temp file
             return
 
         os.rename(temp_file_path, full_model_path)
 
         # Save the config file for the specific downloaded model
-        with open(full_config_path, 'w') as f:
+        with open(full_config_path, "w") as f:
             current_cfg = WhisperSerial(
                 name=model_name,
                 download_url=url,
                 size=get_model_size(full_model_path),
                 checksum=hasher.hexdigest(),
-                config=config.models.whisper.config
+                config=config.models.whisper.config,
             ).model_dump()
             toml.dump(current_cfg, f)
 
-        logging.info(f'Model {model_name} downloaded successfully. \n')
+        logging.info(f"Model {model_name} downloaded successfully. \n")
 
     @classmethod
     def _download_llama_model(
-            cls,
-            model_name: str,
-            config: CFGProgram,
+        cls,
+        model_name: str,
+        config: CFGProgram,
     ):
         pass
 
-
     @classmethod
     def _load_model(
-            cls,
-            model_name: str,
-            config: CFGProgram,
+        cls,
+        model_name: str,
+        config: CFGProgram,
     ) -> Whisper:
         """
         Load a whisper model from the local disk, with default config.
@@ -123,30 +124,28 @@ class ModelConfig:
         :return: A Whisper instance from whispercpp
         """
         # Check if _download_whisper_model has been called before
-        if not os.path.exists(os.path.join(cls.__get_dir(config, 'models'), f'ggml-{model_name}.bin')):
-            logging.info(f'Model {model_name} not found. Downloading it ... \n')
+        if not os.path.exists(os.path.join(cls.__get_dir(config, "models"), f"ggml-{model_name}.bin")):
+            logging.info(f"Model {model_name} not found. Downloading it ... \n")
             cls._download_whisper_model(model_name, config)
 
         # Load the model config for the model_name
-        full_config_path = os.path.join(cls.__get_dir(config, 'config'), f'ggml-{model_name}.toml')
-        full_model_path = os.path.join(cls.__get_dir(config, 'models'), f'ggml-{model_name}.bin')
+        full_config_path = os.path.join(cls.__get_dir(config, "config"), f"ggml-{model_name}.toml")
+        full_model_path = os.path.join(cls.__get_dir(config, "models"), f"ggml-{model_name}.bin")
         if not os.path.exists(full_config_path):
-            logging.error(f'No config file found for model {model_name}. \n')
-            return
+            logging.error(f"No config file found for model {model_name}. \n")
+            sys.exit(1)
 
-        with open(full_config_path, 'r') as f:
+        with open(full_config_path, "r") as f:
             model_config = toml.load(f)
-            _model = Whisper.from_params(
-                model_name=full_model_path,
-                params=WhisperSerial(**model_config).config
-            )
+            _model = Whisper.from_params(model_name=full_model_path, params=WhisperSerial(**model_config).config)
         return _model
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     # How to use the config class
-    with open('../config.toml', 'r') as f:
+    with open("../config.toml", "r") as f:
         config = toml.load(f)
 
     cfg = CFGProgram(**config)
-    model = ModelConfig._load_model('tiny.en', cfg)
+    print(dict(cfg.general.available_whisper_models))
+    # model = ModelConfig._load_model("tiny.en", cfg)
