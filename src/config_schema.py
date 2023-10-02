f54960c47f0248fdee8ac870524288ce5fc237ef
Aymane Hachcham <aymanehachchaming@gmail.com>
2023-10-02 17:02:45 +0200

    schema improved for whisper model

--- src/config_schema.py
+++ src/config_schema.py
@@ -1,20 +1,14 @@
 
 
 from utils import is_valid_url
-from typing import List
+from typing import List, Tuple
 from pydantic import BaseModel, field_validator
 import toml
 
 
 class CFGGeneral(BaseModel):
     root_path: str
-    whisper_url: str
-
-    @field_validator('whisper_url')
-    def validate_whisper_url(cls, url):
-        # assert that the url is valid
-        assert is_valid_url(url), f'Invalid url: {url}'
-        return url
+    available_whisper_models: List[Tuple[str, str]]
 
 class CFGWhisper(BaseModel):
     n_vocab: int
@@ -29,21 +23,30 @@ class CFGWhisper(BaseModel):
     n_mels: int
     f16: int
 
-class Whisper(BaseModel):
+class WhisperSerial(BaseModel):
     name: str
-    version: str
-    download_path: str
-    checksum: str
+    download_url: str
     size: str
-    available_models: List[str]
+    checksum: str
     config: CFGWhisper
 
+    @field_validator('download_url')
+    def validate_whisper_url(cls, url):
+        # assert that the url is valid
+        assert is_valid_url(url), f'Invalid url: {url}'
+        return url
+
 class CFGModel(BaseModel):
-    whisper: Whisper
+    whisper: WhisperSerial
 
 class CFGProgram(BaseModel):
     general: CFGGeneral
     models: CFGModel
+
+if __name__ == '__main__':
+    with open('../config.toml', 'r') as f:
+        config = toml.load(f)
+
 
 
 
